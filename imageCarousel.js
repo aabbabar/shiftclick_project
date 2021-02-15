79072154bff71d819eb4dbde382181ff7a050fe9
aabbabar <azleeb@gmail.com>
2021-02-15 12:15:25 +0000

    refactor changeImg function

--- imageCarousel.js
+++ imageCarousel.js
@@ -4,21 +4,16 @@ const play = document.querySelector('#play');
 const pause = document.querySelector('#pause');
 const next = document.querySelector('#next');
 const imgID = [ '0183', '1049', '1255', '0750', '1093', '1352', '0905', '1096' ];
-
 let currentIndex = 0;
+prev.addEventListener('click', () => changeImg('l'));
+next.addEventListener('click', () => changeImg('r'));
 
-img.src = `/imgs/IMG_${imgID[0]}.JPG`;
-
-function changeImg() {
-    let nextImg = imgID[currentIndex];
-    img.src = `/imgs/IMG_${nextImg}.JPG`;
+function changeImg(direction) {
+  if (direction == 'r') {
+    currentIndex === 7 ? (currentIndex = 0) : currentIndex++;
+  } else if (direction == 'l') {
+    currentIndex === 0 ? (currentIndex = 7) : currentIndex--;
+  }
+  let nextImg = imgID[currentIndex];
+  img.src = `/imgs/IMG_${nextImg}.JPG`;
 }
-
-prev.addEventListener('click', () => {
-    currentIndex = currentIndex !== 0 ? currentIndex - 1 : 7;
-    changeImg();
-});
-next.addEventListener('click', () => {
-    currentIndex = currentIndex !== 7 ? currentIndex + 1 : 0;
-    changeImg();
-});
