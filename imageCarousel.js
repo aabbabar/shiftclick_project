73780726a4425bb9fa59205c7a7d5b9969c0b374
aabbabar <azleeb@gmail.com>
2021-02-15 13:25:28 +0000

    implement autoplay feature

--- imageCarousel.js
+++ imageCarousel.js
@@ -5,8 +5,13 @@ const pause = document.querySelector('#pause');
 const next = document.querySelector('#next');
 const imgID = [ '0183', '1049', '1255', '0750', '1093', '1352', '0905', '1096' ];
 let currentIndex = 0;
+let autoplay;
+
+window.addEventListener('load', autoplayStart);
 prev.addEventListener('click', () => changeImg('l'));
 next.addEventListener('click', () => changeImg('r'));
+play.addEventListener('click', autoplayStart);
+pause.addEventListener('click', autoplayStop);
 
 function changeImg(direction) {
   if (direction == 'r') {
@@ -16,4 +21,12 @@ function changeImg(direction) {
   }
   let nextImg = imgID[currentIndex];
   img.src = `/imgs/IMG_${nextImg}.JPG`;
+}
+
+function autoplayStart() {
+  autoplay = setInterval(() => changeImg('r'), 1250);
+}
+
+function autoplayStop() {
+  clearInterval(autoplay);
 }
