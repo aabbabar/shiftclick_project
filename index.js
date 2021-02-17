bf0ea497f3a441d36dac250860dbf85e5f0538e0
aabbabar <azleeb@gmail.com>
2021-02-17 12:02:35 +0000

    added navbar hover effect

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ const navs = document.querySelectorAll(".navlink");
 const links = [];
 
 for (let i = 0; i < pages.length; i++) {
-  let link = { page: pages[i], nav: navs[i] };
+  let link = { page: pages[i], nav: navs[i], isLive: false };
   links.push(link);
 }
 
+//collapse into one forEach
 pages.forEach((page) => (page.style.display = "none"));
-links.forEach((link) => link.nav.addEventListener("click", () => showPage(link.page)));
+links.forEach((link) => link.nav.addEventListener("click", () => showPage(link)));
+links.forEach((link) => link.nav.addEventListener("mouseover", () => navHover(link, "over")));
+links.forEach((link) => link.nav.addEventListener("mouseout", () => navHover(link, "out")));
 
-function showPage(page) {
+function showPage(link) {
   pages.forEach((page) => (page.style.display = "none"));
-  page.style.display = "block";
+  navs.forEach((nav) => (nav.style.color = "rgb(55, 55, 55)"));
+  navs.forEach((nav) => (nav.style.textDecoration = "none"));
+  links.forEach((link) => (link.isLive = false));
+  link.page.style.display = "block";
+  link.nav.style.color = "rgb(153, 255, 0)";
+  link.nav.style.textDecoration = "underline";
+  link.nav.style.textDecorationStyle = "double";
+  link.isLive = true;
 }
+
+function navHover(link, dir) {
+  if (!link.isLive) {
+    if (dir == "over") link.nav.style.color = "rgb(153, 255, 0)";
+    if (dir == "out") link.nav.style.color = "rgb(55, 55, 55)";
+  }
+}
+
+window.addEventListener("load", () => showPage(links[0]));
