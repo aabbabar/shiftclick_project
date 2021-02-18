0d2cf04be56621959ab86db22c27a7c32f0a9b6d
aabbabar <azleeb@gmail.com>
2021-02-18 17:00:11 +0000

    fixed image links

--- imageCarousel.js
+++ imageCarousel.js
@@ -1,5 +1,5 @@
-const [ prev, autoplay, next ] = document.querySelectorAll(".carouselButton");
-const carouselButtons = [ prev, autoplay, next ];
+const carouselButtons = document.querySelectorAll(".carouselButton");
+const [ prev, autoplay, next ] = carouselButtons;
 const img = document.querySelector("#image");
 const caption = document.querySelector("#caption");
 let currentIndex = 0;
@@ -11,7 +11,6 @@ prev.addEventListener("click", () => arrowButton("l"));
 next.addEventListener("click", () => arrowButton("r"));
 window.addEventListener("load", autoplayStart);
 document.addEventListener("keydown", (keydown) => keyControl(keydown));
-
 carouselButtons.forEach((button) => {
   button.addEventListener("mouseover", () => button.classList.add("carouselButtonHover"));
   button.addEventListener("mouseout", () => button.classList.remove("carouselButtonHover"));
@@ -24,39 +23,19 @@ function changeImg(direction) {
     currentIndex === 0 ? (currentIndex = imgs.length - 1) : currentIndex--;
   }
   let nextID = imgs[currentIndex].id;
-  img.src = `/imgs/fungi/IMG_${nextID}.JPG`;
+  img.src = `./imgs/fungi/IMG_${nextID}.JPG`;
   caption.textContent = imgs[currentIndex].caption;
-}
-
-function arrowButton(direction) {
-  autoplayStop();
-  changeImg(direction);
-}
-
-function keyControl(keydown) {
-  if (keydown.key == "ArrowLeft") {
-    arrowButton("l");
-    flash(prev);
-  }
-  if (keydown.key == "ArrowRight") {
-    arrowButton("r");
-    flash(next);
-  }
-  if (keydown.key == " ") {
-    toggleAutoplay();
-    flash(autoplay);
-  }
 }
 
 function autoplayStart() {
   autoplayStop();
   autoplayer = setInterval(() => changeImg("r"), 3000);
-  autoplay.src = "/imgs/icons/autoplay-on.png";
+  autoplay.src = "./imgs/icons/autoplay-on.png";
   isAutoplay = true;
 }
 
 function autoplayStop() {
-  autoplay.src = "/imgs/icons/autoplay-off.png";
+  autoplay.src = "./imgs/icons/autoplay-off.png";
   clearInterval(autoplayer);
   isAutoplay = false;
 }
@@ -65,38 +44,66 @@ function toggleAutoplay() {
   isAutoplay ? autoplayStop() : autoplayStart();
 }
 
+function arrowButton(direction) {
+  autoplayStop();
+  changeImg(direction);
+}
+
 function flash(button) {
   button.classList.add("carouselButtonHover");
   setTimeout(() => button.classList.remove("carouselButtonHover"), 150);
 }
 
+function keyControl(keydown) {
+  switch (keydown.key) {
+    case "ArrowLeft":
+      arrowButton("l");
+      flash(prev);
+      break;
+    case "ArrowRight":
+      arrowButton("r");
+      flash(next);
+      break;
+    case " ":
+      toggleAutoplay();
+      flash(autoplay);
+  }
+}
+
 const imgs = [
   {
     id      : "0183",
-    caption : "Pearl Oysters (Pleurotus ostreatus) growing in spiral formations"
+    caption : "Pearl Oysters (Pleurotus ostreatus) growing in spiral formations",
+    alt     : "Pearl Oysters"
   },
   {
     id      : "1049",
-    caption : "A convergent growth of Amber Jelly fungus (Exidia recisia)"
+    caption : "A convergent growth of Amber Jelly fungus (Exidia recisia)",
+    alt     : "Amber Jelly Fungus"
   },
   {
     id      : "1255",
-    caption : "An Artist's Conk (Ganoderma applanatum) covered in yellow flat-footed fly galls"
+    caption : "An Artist's Conk (Ganoderma applanatum) covered in yellow flat-footed fly galls",
+    alt     : "Artist's Conk"
   },
   {
     id      : "0750",
-    caption : "A fully unfurled Collared Earthstar (Geastrum triplex)"
+    caption : "A fully unfurled Collared Earthstar (Geastrum triplex)",
+    alt     : "Collared Earthstar"
   },
   {
     id      : "1352",
-    caption : 'Yellow Brain fungus (Tremella mesenterica) also known as "witches\' butter"'
+    caption : 'Yellow Brain fungus (Tremella mesenterica) also known as "witches\' butter"',
+    alt     : "Yellow Brain fungus"
   },
   {
     id      : "0905",
-    caption : "Labyrinthian patterns on underside of a Blushing Bracket (Daedeleopsis confragosa)"
+    caption : "Labyrinthian patterns on underside of a Blushing Bracket (Daedeleopsis confragosa)",
+    alt     : "Blushing Bracket"
   },
   {
     id      : "1096",
-    caption : "A small congregation of Shaggy Scalycaps (Pholiota squarrosa)"
+    caption : "A small congregation of Shaggy Scalycaps (Pholiota squarrosa)",
+    alt     : "Shaggy Scalycaps"
   }
 ];
